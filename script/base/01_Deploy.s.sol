91d0fd31a81dac74bbda8f5b2e846e258985a663
fepvenancio <filipeepv@gmail.com>
2025-03-28 13:32:52 +0000

    fix: vault staleness check, also 01_Deploy script

--- script/base/01_Deploy.s.sol
+++ script/base/01_Deploy.s.sol
@@ -1,30 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {
-    DivestSuperform, InvestSuperform, LiquidateSuperform, SuperformGateway
-} from "crosschain/SuperformGateway/Lib.sol";
-import { Script, console2 } from "forge-std/Script.sol";
+import {DivestSuperform, InvestSuperform, LiquidateSuperform, SuperformGateway} from "crosschain/SuperformGateway/Lib.sol";
+import {Script, console2} from "forge-std/Script.sol";
 
-import { SUPERFORM_ROUTER_BASE, SUPERFORM_SUPERPOSITIONS_BASE, USDCE_BASE, WETH_BASE } from "helpers/AddressBook.sol";
-import {
-    IBaseRouter,
-    IHurdleRateOracle,
-    IMetaVault,
-    ISharePriceOracle,
-    ISuperPositions,
-    ISuperformFactory,
-    ISuperformGateway
-} from "interfaces/Lib.sol";
-import {
-    AssetsManager,
-    ERC7540Engine,
-    ERC7540EngineReader,
-    ERC7540EngineSignatures,
-    MetaVaultReader
-} from "modules/Lib.sol";
-import { MetaVault } from "src/MetaVault.sol";
-import { VaultConfig } from "types/Lib.sol";
+import {SUPERFORM_ROUTER_BASE, SUPERFORM_SUPERPOSITIONS_BASE, USDCE_BASE, WETH_BASE} from "helpers/AddressBook.sol";
+import {IBaseRouter, IHurdleRateOracle, IMetaVault, ISharePriceOracle, ISuperPositions, ISuperformFactory, ISuperformGateway} from "interfaces/Lib.sol";
+import {AssetsManager, EmergencyAssetsManager, ERC7540Engine, ERC7540EngineReader, ERC7540EngineSignatures, MetaVaultReader} from "modules/Lib.sol";
+import {MetaVault} from "src/MetaVault.sol";
+import {VaultConfig} from "types/Lib.sol";
 
 contract DeployScript is Script {
     VaultConfig public config;
@@ -34,6 +18,7 @@ contract DeployScript is Script {
     ERC7540EngineReader public engineReader;
     ERC7540EngineSignatures public engineSignatures;
     AssetsManager public assetManager;
+    EmergencyAssetsManager public emergencyAssetsManager;
     MetaVaultReader public metaVaultReader;
 
     uint256 deployerPrivateKey;
@@ -62,7 +47,7 @@ contract DeployScript is Script {
             performanceFee: 2000,
             oracleFee: 50,
             hurdleRateOracle: IHurdleRateOracle(hurdleRateOracleAddress),
-            sharesLockTime: 30 days,
+            sharesLockTime: 0,
             superPositions: ISuperPositions(SUPERFORM_SUPERPOSITIONS_BASE),
             treasury: makeAddr("treasury"),
             signerRelayer: relayerRole,
@@ -92,7 +77,9 @@ contract DeployScript is Script {
 
         // Set gatway
         vault.setGateway(address(gateway));
-
+        // Set the Dust Threshold
+        vault.setDustThreshold(3000000);
+        
         // Add vault modules
         engine = new ERC7540Engine();
         bytes4[] memory engineSelectors = engine.selectors();
@@ -103,16 +90,34 @@ contract DeployScript is Script {
         vault.addFunctions(engineReaderSelectors, address(engineReader), false);
 
         engineSignatures = new ERC7540EngineSignatures();
-        bytes4[] memory engineSignaturesSelectors = engineSignatures.selectors();
-        vault.addFunctions(engineSignaturesSelectors, address(engineSignatures), false);
+        bytes4[] memory engineSignaturesSelectors = engineSignatures
+            .selectors();
+        vault.addFunctions(
+            engineSignaturesSelectors,
+            address(engineSignatures),
+            false
+        );
 
         assetManager = new AssetsManager();
         bytes4[] memory assetManagerSelectors = assetManager.selectors();
         vault.addFunctions(assetManagerSelectors, address(assetManager), false);
 
+        emergencyAssetsManager = new EmergencyAssetsManager();
+        bytes4[] memory emergencyAssetsManagerSelectors = emergencyAssetsManager
+            .selectors();
+        vault.addFunctions(
+            emergencyAssetsManagerSelectors,
+            address(emergencyAssetsManager),
+            false
+        );
+
         metaVaultReader = new MetaVaultReader();
         bytes4[] memory metaVaultReaderSelectors = metaVaultReader.selectors();
-        vault.addFunctions(metaVaultReaderSelectors, address(metaVaultReader), false);
+        vault.addFunctions(
+            metaVaultReaderSelectors,
+            address(metaVaultReader),
+            false
+        );
 
         // Grant roles
         vault.grantRoles(managerAddressRole, vault.MANAGER_ROLE());
@@ -123,7 +128,10 @@ contract DeployScript is Script {
         console2.log("Gateway deployed at: ", address(gateway));
         console2.log("Engine deployed at: ", address(engine));
         console2.log("EngineReader deployed at: ", address(engineReader));
-        console2.log("EngineSignatures deployed at: ", address(engineSignatures));
+        console2.log(
+            "EngineSignatures deployed at: ",
+            address(engineSignatures)
+        );
         console2.log("AssetManager deployed at: ", address(assetManager));
         console2.log("MetaVaultReader deployed at: ", address(metaVaultReader));
         console2.log("InvestSuperform deployed at: ", address(invest));
