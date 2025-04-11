5b0dc5c88e3c5f1101a377b8d0a786dc03053d6a
v3n.gno <filipeepv@gmail.com>
2025-04-11 09:03:59 +0100

    fix: imports and scripts (#46)

--- script/base/06_AddFunctions.s.sol
+++ script/base/06_AddFunctions.s.sol
@@ -13,21 +13,39 @@ contract AddFunctionsScript is Script {
     AssetsManager public assetsManager;
 
     function run() public {
+        console2.log("Starting function addition process...");
+
         adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
         metavault = IMetaVault(vm.envAddress("METAVAULT_ADDRESS"));
         vm.startBroadcast(adminPrivateKey);
 
+        console2.log("Deploying new modules...");
         engine = new ERC7540Engine();
-        //assetsManager = new AssetsManager();
+        assetsManager = new AssetsManager();
 
         bytes4[] memory engineSelectors = engine.selectors();
-        //bytes4[] memory assetsSelectors = assetsManager.selectors();
+        bytes4[] memory assetsSelectors = assetsManager.selectors();
 
+        console2.log("Adding functions to MetaVault at:", address(metavault));
+
+        console2.log("Adding ERC7540Engine functions...");
+        console2.log("Number of selectors:", engineSelectors.length);
+        for (uint256 i = 0; i < engineSelectors.length; i++) {
+            console2.log(" - Selector:", uint32(engineSelectors[i]));
+        }
         metavault.addFunctions(engineSelectors, address(engine), false);
-        //metavault.addFunctions(assetsSelectors, address(assetsManager), false);
+        console2.log("ERC7540Engine functions added successfully");
 
-        console2.log("ERC7540Engine address: ", address(engine));
-        //console2.log("AssetsManager address: ", address(assetsManager));
+        console2.log("Adding AssetsManager functions...");
+        console2.log("Number of selectors:", assetsSelectors.length);
+        for (uint256 i = 0; i < assetsSelectors.length; i++) {
+            console2.log(" - Selector:", uint32(assetsSelectors[i]));
+        }
+        metavault.addFunctions(assetsSelectors, address(assetsManager), true);
+        console2.log("AssetsManager functions added successfully");
+
+        console2.log("ERC7540Engine deployed at:", address(engine));
+        console2.log("AssetsManager deployed at:", address(assetsManager));
 
         vm.stopBroadcast();
     }
--- script/base/07_RemoveFunctionsGateway.s.sol
+++ script/base/07_RemoveFunctionsGateway.s.sol
@@ -18,8 +18,12 @@ contract RemoveFunctionsScript is Script {
     LiquidateSuperform liquidate;
 
     function run() public {
+        console2.log("Starting gateway function removal process...");
+
         adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
         gateway = ISuperformGateway(payable(vm.envAddress("SUPERFORM_GATEWAY_ADDRESS")));
+
+        console2.log("Removing functions from Gateway at:", address(gateway));
 
         vm.startBroadcast(adminPrivateKey);
 
@@ -31,9 +35,29 @@ contract RemoveFunctionsScript is Script {
         bytes4[] memory divestSelectors = divest.selectors();
         bytes4[] memory liquidateSelectors = liquidate.selectors();
 
+        console2.log("Removing InvestSuperform functions...");
+        console2.log("Number of selectors:", investSelectors.length);
+        for (uint256 i = 0; i < investSelectors.length; i++) {
+            console2.log(" - Selector:", uint32(investSelectors[i]));
+        }
         gateway.removeFunctions(investSelectors);
+        console2.log("InvestSuperform functions removed successfully");
+
+        console2.log("Removing DivestSuperform functions...");
+        console2.log("Number of selectors:", divestSelectors.length);
+        for (uint256 i = 0; i < divestSelectors.length; i++) {
+            console2.log(" - Selector:", uint32(divestSelectors[i]));
+        }
         gateway.removeFunctions(divestSelectors);
+        console2.log("DivestSuperform functions removed successfully");
+
+        console2.log("Removing LiquidateSuperform functions...");
+        console2.log("Number of selectors:", liquidateSelectors.length);
+        for (uint256 i = 0; i < liquidateSelectors.length; i++) {
+            console2.log(" - Selector:", uint32(liquidateSelectors[i]));
+        }
         gateway.removeFunctions(liquidateSelectors);
+        console2.log("LiquidateSuperform functions removed successfully");
 
         vm.stopBroadcast();
     }
